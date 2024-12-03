de39659f4e1298fb77dbf67ce232c3d319753018
mungai michael <83639621+mungaimichael@users.noreply.github.com>
2024-12-03 19:51:19 +0300

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_lessons/screens/home.dart';
+
 
 void main () {
   runApp(const MaterialApp(
@@ -11,36 +12,5 @@ void main () {
 }
 
 
-class Home extends StatelessWidget {
-  const Home ({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        systemOverlayStyle: const SystemUiOverlayStyle(
-            statusBarColor: Colors.transparent
 
-        ),
-          backgroundColor: Colors.transparent,
-          centerTitle: true,
-          bottomOpacity: 3.5,
-          title: const Text("Home"),
-          titleTextStyle: const TextStyle(
-              fontSize: 26,
-              fontFamily: "Montserrat"
-          ),
-      ),
-          body: Container(
-            color: Colors.white,
-            height: double.infinity ,
-            width: double.infinity,
-            child: const Text("Hello World", style: TextStyle(
-                  fontSize: 30,
-                  decoration: TextDecoration.underline
-            ),),
-          )
-    );
-  }
-}
