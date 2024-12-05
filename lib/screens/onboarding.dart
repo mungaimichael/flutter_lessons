618cf9259d80259e3640a41376b5c0cd7c9b67d1
mungai michael <83639621+mungaimichael@users.noreply.github.com>
2024-12-05 16:48:17 +0300

    refactored app bar to custom widget

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_lessons/screens/home.dart';
+import 'package:flutter_lessons/widgets/AppbarWidget.dart';
 import 'package:flutter_lessons/widgets/Wrapper.dart';
-import 'package:google_fonts/google_fonts.dart';
+
+import 'login.dart';
 
 class Onboarding extends StatelessWidget {
   const Onboarding ({super.key});
@@ -10,45 +11,8 @@ class Onboarding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          systemOverlayStyle: const SystemUiOverlayStyle(
-              statusBarColor: Colors.black
-
-          ),
-          backgroundColor: Colors.white,
-          centerTitle: true,
-          bottomOpacity: 3.5,
-          title: Row(
-
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 30,
-                height: 30,
-                child: Image.asset(
-                  "assets/logo.png",
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("TaskTugas",
-                  style: GoogleFonts.poppins(
-                    fontSize: 24,
-                  ),
-
-                ),
-              ),
-            ],
-          ),
-          titleTextStyle: const TextStyle(
-              fontSize: 26,
-              color: Colors.black,
-              fontWeight: FontWeight.bold
-          ),
-        ),
-        body: const Wrapper(text1: "Be a Flutter Ninja Faster than Ever", text2: "Flutter provides a complete system for navigating between screens and handling deep links. Small applications without complex deep linking can use Navigator", img: '2', nextScreen: Home()),
+        appBar: const AppbarWidget(),
+        body: Wrapper(text1: "Be a Flutter Ninja Faster than Ever", text2: "Flutter provides a complete system for navigating between screens and handling deep links. Small applications without complex deep linking can use Navigator", img: '2', nextScreen: Login(), skip: Login(),),
     );
   }
 }
