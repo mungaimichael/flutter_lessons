51e1635c3129b139b07417745a354dc9d17c040a
mungai michael <83639621+mungaimichael@users.noreply.github.com>
2024-12-11 12:34:50 +0300

    route refactor

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lessons/screens/onboarding.dart';
 import 'package:flutter_lessons/widgets/AppbarWidget.dart';
 import 'package:flutter_lessons/widgets/Wrapper.dart';
 
@@ -16,7 +15,7 @@ class Home extends StatelessWidget {
         appBar:  const AppbarWidget(),
         body: Wrapper(
           text1: "Make Learning Flutter Easy, Fast and Fun"
-        , text2: "When someone learning Flutter asks you why some widget with width: 100 isn't 100 pixels wide", img: 'onboarding', nextScreen: const Onboarding(), skip: Login(), ),
+        , text2: "When someone learning Flutter asks you why some widget with width: 100 isn't 100 pixels wide", img: 'onboarding', nextScreen: "onboarding", skip: Login(), ),
 
     );
   }
