5b340c164f4bb746c8107d2781cddc503482ea99
mungai michael <83639621+mungaimichael@users.noreply.github.com>
2024-12-22 15:26:01 +0300

    products page redesign

--- lib/screens/products.dart
+++ lib/screens/products.dart
@@ -1,86 +1,252 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_lessons/screens/product.dart';
-import 'package:flutter_lessons/widgets/ProductItem.dart';
 import 'package:get/get.dart';
-
-
+import '../screens/product.dart';
 import '../util/dio_functions.dart';
 
-Future<List<dynamic>> getProducts() async {
-  try{
-    var response = await dio.get("https://fakestoreapi.com/products");
-    if (response.statusCode == 200){
-      return response.data;
-    }
-    else {
-      throw Exception("Failed to fetch products with code ${response.statusCode}");
-    }
-  }
-  catch(e){
-    throw Exception("Failed to fetch products with error $e");
-  }
-}
-
-class Products  extends StatefulWidget {
-  const Products({super.key});
+class ProductsScreen extends StatefulWidget {
+  const ProductsScreen({super.key});
 
   @override
-  _ScreenState createState() => _ScreenState();
-
+  _ProductsScreenState createState() => _ProductsScreenState();
 }
 
-
-class _ScreenState extends State<Products> {
-  late Future<List<dynamic>> dataFuture;
+class _ProductsScreenState extends State<ProductsScreen> {
+  late Future<List<dynamic>> _productsFuture;
 
   @override
   void initState() {
     super.initState();
-    dataFuture = getProducts();
+    _productsFuture = _fetchProducts();
   }
+
+  Future<List<dynamic>> _fetchProducts() async {
+    try {
+      final response = await dio.get("https://fakestoreapi.com/products");
+      if (response.statusCode == 200) {
+        return response.data;
+      }
+      throw Exception("Failed to fetch products: ${response.statusCode}");
+    } catch (e) {
+      throw Exception("Network error: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: FutureBuilder<List<dynamic>>(
-        future: dataFuture,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: const Text(
+          'Products',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: FutureBuilder<List<dynamic>>(
+        future: _productsFuture,
         builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-
-                return
-                  GestureDetector(
-                      onTap: () {
-                        Get.to(() => ProductPage(productId: snapshot.data![index]['id']));
-                      },
-                      child: ProductItem
-                  (
-                  title: snapshot.data![index]['title'].toString(),
-                  image: snapshot.data![index]['image'].toString(),
-                  price: snapshot.data![index]['price'].toString(),
-                ));
-              },
-            );
-          } else if (snapshot.hasError) {
-            return Text("${snapshot.error}");
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const _LoadingView();
           }
-          // By default, show a loading spinner.
-          return const Center(child:  Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:  [
-              CircularProgressIndicator(
-                color: Colors.orangeAccent,
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 8.0),
-                child: Text("Loading..."),
-              ),
-            ],
-          ));
+
+          if (snapshot.hasError) {
+            return _ErrorView(error: snapshot.error.toString());
+          }
+
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const _EmptyView();
+          }
+
+          return _ProductListView(products: snapshot.data!);
         },
+      ),
+    );
+  }
+}
+
+class _ProductListView extends StatelessWidget {
+  final List<dynamic> products;
+
+  const _ProductListView({required this.products});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: const EdgeInsets.all(16),
+      itemCount: products.length,
+      separatorBuilder: (context, index) => const SizedBox(height: 16),
+      itemBuilder: (context, index) {
+        final product = products[index];
+        return _ProductCard(
+          id: product['id'],
+          title: product['title'].toString(),
+          image: product['image'].toString(),
+          price: product['price'].toString(),
+        );
+      },
+    );
+  }
+}
+
+class _ProductCard extends StatelessWidget {
+  final int id;
+  final String title;
+  final String image;
+  final String price;
+
+  const _ProductCard({
+    required this.id,
+    required this.title,
+    required this.image,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Get.to(() => ProductPage(productId: id)),
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey.shade200),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade50,
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: Image.network(
+                image,
+                fit: BoxFit.contain,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) return child;
+                  return Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.grey.shade300,
+                      strokeWidth: 2,
+                    ),
+                  );
+                },
+                errorBuilder: (context, error, stackTrace) {
+                  return Icon(
+                    Icons.image_not_supported_outlined,
+                    color: Colors.grey.shade400,
+                    size: 32,
+                  );
+                },
+              ),
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontSize: 15,
+                      color: Colors.black87,
+                      height: 1.4,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    '\$${double.parse(price).toStringAsFixed(2)}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Icon(
+              Icons.chevron_right,
+              color: Colors.grey.shade400,
+              size: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _LoadingView extends StatelessWidget {
+  const _LoadingView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator(
+        color: Colors.grey.shade300,
+        strokeWidth: 2,
+      ),
+    );
+  }
+}
+
+class _ErrorView extends StatelessWidget {
+  final String error;
+
+  const _ErrorView({required this.error});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.error_outline,
+              color: Colors.grey.shade400,
+              size: 32,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Unable to load products',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey.shade600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _EmptyView extends StatelessWidget {
+  const _EmptyView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'No products available',
+        style: TextStyle(
+          fontSize: 16,
+          color: Colors.grey.shade600,
+        ),
       ),
     );
   }
